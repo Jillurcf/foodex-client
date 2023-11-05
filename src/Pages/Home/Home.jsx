3e52689ae2f737aec25680d11da2aeba2933e8a4
jillurcf <jrtushar@gmail.comm>
2023-11-04 19:52:14 -0700

    Helmet provider added successfully

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+            <title>FoodHero | Home</title>
+            </Helmet>
             <h2 className="text-3xl">Home page</h2>
         </div>
     );
