44d74303c1d52625f304993311a345d4c1f144d7
jillurcf <jrtushar@gmail.comm>
2023-11-05 21:14:09 -0800

    fake json data created for All food Item

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,8 +8,8 @@ import UseAuth from "../../Hooks/UseAuth";
 const Home = () => {
     const {loading} = UseAuth();
     if(loading){
-        // return <progress className="progress w-56"></progress>
-      return  <span className="loading loading-spinner loading-lg"></span>
+        return <progress className="progress w-56"></progress>
+    //   return  <span className="loading loading-spinner loading-lg"></span>
     }
     return (
         
