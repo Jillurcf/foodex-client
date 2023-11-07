c10569dd266e55482335d47b96b3105445447f07
jillurcf <jrtushar@gmail.comm>
2023-11-06 17:59:00 -0800

    Search functionality added to the All Food page

--- src/Pages/AllFoodItem/AllFoodCard.jsx
+++ src/Pages/AllFoodItem/AllFoodCard.jsx
@@ -3,31 +3,30 @@ import { useEffect, useState } from "react";
 const AllFoodCard = ({ food }) => {
   const { food_image, food_name, food_category, price, quantity } = food;
 
-
-
-  return (  
+  return (
     <div className="">
-       
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-          <img
-            src={food_image}
-            className="w-1/2 rounded-lg shadow-2xl"
-          />
+          <img src={food_image} className="w-1/2 rounded-lg shadow-2xl" />
           <div>
             <h1 className="text-2xl text-pink-500 font-bold">{food_name}</h1>
             <p className="py-6 text-blue-500 font-bold text-xl">
               {food_category}
             </p>
             <div className="flex justify-between">
-                <button className="btn btn-xs bg-blue-500 text-white">Price: ${price}</button>
-                <button className="btn btn-xs btn-outline border-pink-500">Quantity: {quantity}</button>
+              <button className="btn btn-xs bg-blue-500 text-white">
+                Price: ${price}
+              </button>
+              <button className="btn btn-xs btn-outline border-pink-500">
+                Quantity: {quantity}
+              </button>
             </div>
-            <button className="mt-12 btn btn-outline btn-secondary font-bold mx-auto flex w-full">See Details</button>
+            <button className="mt-12 btn btn-outline btn-secondary font-bold mx-auto flex w-full">
+              See Details
+            </button>
           </div>
         </div>
       </div>
-     
     </div>
   );
 };
--- src/Pages/Home/BookATable.jsx
+++ src/Pages/Home/BookATable.jsx
@@ -1,11 +1,11 @@
 const BookATable = () => {
   return (
-    <div className="my-8">
+    <div className="my-36">
       <div className="card w-1/2 flex mx-auto bg-neutral text-neutral-content">
         <div className="card-body items-center text-center">
           <h2 className="card-title text-white text-4xl fon-bold">BooK A Table</h2>
          <div className="">
-         <input type="number" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs" />
+         <input type="number" placeholder="Type here" className=" input input-info w-full max-w-xs" />
          <input type="date" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs" />
          <input type="time" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs" />
             
